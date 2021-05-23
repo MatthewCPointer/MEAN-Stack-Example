2bd96de7603fb5e8dc96e43628dcac838d97f879
Matthew Pointer <pointermatthew@gmail.com>
2021-05-23 09:53:26 -0400

    Adding sample path /api/posts to test express app

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts' ,(req, res, next) => {
+  const posts = [
+    {
+      id: 'asdfasd123',
+      title: 'This is our first server-side post',
+      content: 'This is data coming from the node server'
+    },
+    {
+      id: 'asdfasd132',
+      title: 'This is our second server-side post',
+      content: 'This is data coming from the node server'
+    }
+  ];
+  res.status(200).json({
+    msg: 'Posts Fetch Success',
+    posts: posts
+  });
 });
 
 module.exports = app;
